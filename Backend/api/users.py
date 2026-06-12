438744a7e945b8a79bb90e685ac1771a234ca4be
VolodymyrVH <gorelovv515@gmail.com>
2026-06-12 17:16:34 +0200

    Added GET, PATCH and DELETE enpoints for users

--- Backend/api/users.py
+++ Backend/api/users.py
@@ -23,8 +23,20 @@ def get_by_email(email: str, db: Session = Depends(get_db)):
     return user_db
 
 
-#get_current_user_profile
-#get_by_id
+@router.get("/id/{id}", response_model=UserResponseSchema)
+def get_by_id(user_id: int, db: Session = Depends(get_db)):
+    user_db = db.query(User).filter(User.id == user_id).first()
+    if not user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return user_db
+
+
+@router.get("/me", response_model=UserResponseSchema)
+def get_current_user_profile(current_user: User = Depends(get_current_user)):
+    return current_user
+
+
 @router.post("/", response_model=UserResponseSchema)
 def create_user(user: UserCreateSchema, db: Session = Depends(get_db)):
     existing = db.query(User).filter((User.email == user.email)).first()
@@ -41,5 +53,37 @@ def create_user(user: UserCreateSchema, db: Session = Depends(get_db)):
     return user_db
 
 
-#update_user
-#delete_user
+@router.patch("/{user_id}", response_model=UserResponseSchema)
+def update_user(user_id: int, user_update: UserUpdateSchema, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    if current_user.id != user_id:
+        raise HTTPException(status_code=403, detail="Not enough permession")
+    
+    user_db = db.query(User).filter(User.id == user_id).first()
+    if not user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    update_data = user_update.model_dump(exclude_unset=True)
+
+    if "password" in update_data:
+        update_data["password"] = get_password_hash(update_data.pop("password"))
+
+    for field, value in update_data.items():
+        setattr(user_db, field, value)
+
+    db.commit()
+    db.refresh(user_db)
+
+    return user_db
+
+
+@router.delete("/{user_id}", status_code=204)
+def delete_user(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    if current_user.id != user_id:
+        raise HTTPException(status_code=403, detail="Not enough permession")
+    
+    user_db = db.query(User).filter(User.id == user_id).first()
+    if not user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    db.delete(user_db)
+    db.commit()

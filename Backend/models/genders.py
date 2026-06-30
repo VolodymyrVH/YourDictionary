d590b88d381e8e4eb844822264abe8c75b295f80
VolodymyrVH <gorelovv515@gmail.com>
2026-06-30 15:15:45 +0200

    Implement Gender API endpoints and update relationships in the Gender model

--- Backend/models/genders.py
+++ Backend/models/genders.py
@@ -10,4 +10,4 @@ class Gender(Base):
     id = Column(Integer, primary_key=True)
     gender = Column(String(50), nullable=False)
 
-    words = relationship("Word", back_populates="genders")
+    words = relationship("Word", back_populates="gender")
